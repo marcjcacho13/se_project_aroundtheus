eafc6ee80b6e99ee2f2289613e2fb68d0d97dfdc
marcjacho13 <marcjcacho13@gmail.com>
2024-10-02 21:58:47 -0700

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,34 @@ const initialCards = [
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector('#profile-edit-modal');
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
+const profileEditForm = profileEditModal.querySelector('.modal__form');
 
+/*Functions*/
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+/*Event Handlers*/
+function handleProfileEditSubmit(e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+};
+
+
+/*Event Listeners*/
 profileEditBtn.addEventListener('click', () => {
-    profileEditModal.classList.add('modal_opened');
+profileTitleInput.value = profileTitle.textContent;
+profileDescriptionInput.value = profileDescription.textContent;
+    profileEditModal.classList.add("modal_opened");
 })
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener('submit', handleProfileEditSubmit);
